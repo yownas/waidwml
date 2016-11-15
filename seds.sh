a2d7812c59b4327e058593810e0ebf6ad5c8d772
Jonas Stare <yownas@gmail.com>
2016-11-15 12:11:39 +0100

    Add LANG=C and just cat the wordlist, no grepping

--- seds.sh
+++ seds.sh
@@ -3,10 +3,12 @@
 # Search wordlist for words that works as a sed substitution command and the words that gets
 # changed into words that exists in that list.
 
+LANG=C
+
 words=/usr/share/dict/words
 
-for s in $(grep '^[a-z]*$' $words | grep -P '^s(.)((?!\1).)+\1((?!\1).)*\1$')
+for s in $(cat $words | grep -P '^s(.)((?!\1).)+\1((?!\1).)*\1[gim]*$')
 do
         echo "### $s"
-        (grep '^[a-z]*$' $words | sed 's/^/~ /' ; grep $(echo $s | sed 's/^s\(.\)\(.*\)\1.*\1$/\2/') $words | grep '^[a-z]*$' | sed "h;$s;x;p;x" | awk '{printf $0" ";getline;print}' ) | sort -k 2 | uniq -cf 1 | grep '^\s*2' | awk '{print $2" -> "$3}' | sed "s/^/$s /"
+        (cat $words | sed 's/^/~ /' ; grep $(echo $s | sed 's/^s\(.\)\(.*\)\1.*\1$/\2/') $words | grep '^[a-z]*$' | sed "h;$s;x;p;x" | awk '{printf $0" ";getline;print}' ) | sort -k 2 | uniq -cf 1 | grep '^\s*2' | awk '{print $2" -> "$3}' | sed "s/^/$s /"
 done

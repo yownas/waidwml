295b89eaaa2b22c32944326487499e5b9156983b
Jonas Stare <yownas@gmail.com>
2016-11-15 20:46:33 +0100

    Sometimes words changes words into the same words as other words.

--- seds.sh
+++ seds.sh
@@ -10,5 +10,5 @@ words=/usr/share/dict/words
 for s in $(cat $words | grep -P '^s(.)((?!\1).)+\1((?!\1).)*\1[gim]*$')
 do
         echo "### $s"
-        (cat $words | sed 's/^/~ /' ; grep $(echo $s | sed 's/^s\(.\)\(.*\)\1.*\1$/\2/') $words | grep '^[a-z]*$' | sed "h;$s;x;p;x" | awk '{printf $0" ";getline;print}' ) | sort -k 2 | uniq -cf 1 | grep '^\s*2' | awk '{printf("%-50s %-25s\n", "echo "$2" | sed '$s'", "# "$3)}'
+        (cat $words | sed 's/^/~ /' ; grep $(echo $s | sed 's/^s\(.\)\(.*\)\1.*\1$/\2/') $words | grep '^[a-z]*$' | sed "h;$s;x;p;x" | awk '{printf $0" ";getline;print}' ) | sort -k 2 | uniq -cf 1 | grep '^\s*[2-9]' | awk '{printf("%-50s %-25s\n", "echo "$2" | sed '$s'", "# "$3)}'
 done
